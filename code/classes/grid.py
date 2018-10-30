ad3e7bedc6cc0994bcfc2ffb704222ddb2e589e1
ertzor <43470763+ertzor@users.noreply.github.com>
2018-10-30 22:28:58 +0100

    Update grid.py

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -1,1 +1,11 @@
+class Grid(object):
+    """
+    Representation of the grid in Rush Hour.
+    """
 
+    def __init__(self, size):
+        """
+        Initialize an Item
+        give it a name, description and an initial room id
+        """
+        self.size = size
