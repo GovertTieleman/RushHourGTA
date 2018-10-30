348a154dced8e8a7ad60e91ec77409a26bf645ed
ertzor <43470763+ertzor@users.noreply.github.com>
2018-10-30 22:30:36 +0100

    Update car.py

--- code/classes/car.py
+++ code/classes/car.py
@@ -1,1 +1,13 @@
+class Car(object):
+    """
+    Representation of a car in Rush Hour
+    """
 
+    def __init__(self, length, direction, nose_position):
+        """
+        Initialize an Item
+        give it a name, description and an initial room id
+        """
+        self.length = length
+        self.direction = direction
+        self.nose_position = nose_position
