13d3677122c87bbbb4282444dc01af2c9f773136
10778403 <43132917+10778403@users.noreply.github.com>
2018-11-01 13:38:35 +0100

    Update grid.py
    Wat ideeën toegevoegd voor de grid class

--- code/classes/grid.py
+++ code/classes/grid.py
@@ -3,9 +3,32 @@ class Grid(object):
     Representation of the grid in Rush Hour.
     """
 
-    def __init__(self, size):
+    def __init__(self, height, width):
         """
-        Initialize an Item
-        give it a name, description and an initial room id
+        Define grid and create a list for cars
         """
-        self.size = size
+        self.height = height
+        self.width = width
+        self.coordinates = self.getCoordinates(height, width)
+        self.cars = []
+        
+    def self.getCoordinates(height, width):
+        coordinates = []
+        for x in width:
+            for y in height:
+                coordinates.append(Coordinate(x,y))
+        return coordinates
+
+class Coordinate(object:
+    """
+    Coordinates for the grid
+    """
+                 
+    def __init__(self, x, y):
+        self.coordinate = (x,y)
+        self.occupiedBy = "" #auto naam of index?
+    
+    # Zo'n soort functie kan handig zijn
+    def self.makeSpace()
+        car = self.occupiedBy
+        car.move()
