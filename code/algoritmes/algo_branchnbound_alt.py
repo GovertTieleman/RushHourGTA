41d7e08cc5cf4b11496686bf16c2f1688ea39fec
Fabian <fabiandemoel@gmail.com>
2018-12-04 16:53:44 +0100

    merge for DF,

--- code/algoritmes/algo_branchnbound_alt.py
+++ code/algoritmes/algo_branchnbound_alt.py
@@ -2,7 +2,7 @@
 import sys
 sys.path.insert(0, '../')
 
-from classes.rush_hour_depth_first import RushHour
+from classes.rush_hour_depth_first_alt import RushHour
 
 class Algorithm(object):
     """
@@ -23,13 +23,13 @@ class Algorithm(object):
         self.archive[current_board] = move_count
 
         # Update moves and count
-        move_list = self.game.find_moves(self.game.board, self.game.cars)
+        move_list = self.game.find_moves()
         print(f"{current_board}:\nmove count:{move_count}\narchive size:{len(self.archive)}\n")
         #move_list.reverse()
 
         # Check limit
         move_count += 1
-        if move_count == 15:
+        if move_count == 33:
             return False
 
         # Iterate over valid moves
@@ -61,7 +61,7 @@ class Algorithm(object):
 if __name__ == "__main__":
 
     # Initialize Algorithm
-    game = RushHour("../../data/Game2.txt")
+    game = RushHour("../../data/Game1.txt")
     solution = Algorithm(game)
     count = 0
     print(solution.game.board)
